684d3ce8155553a47c99945cda44e1b67ce2255e
vocongtuanloc <vocong.tuanloc@gmail.com>
2024-05-22 17:13:54 +0700

    ada and trip

--- cplusplus/000066_ada_and_trip.cpp
+++ cplusplus/000066_ada_and_trip.cpp
@@ -8,54 +8,82 @@
 // Topic: Dijkstra
 
 /**
- * Sắp đến 20/10 rồi, Bờm có kế hoạch đi về nhà thăm mẹ của mình. Đất nước mà Bờm đang sống có N thành phố. Hiện Bờm đang học IT ở thành phố 1,
- * và nhà của Bờm thì ở thành phố N. Có M tuyến đường cao tốc, mỗi tuyến đường có một độ dài nhất định và cho phép di chuyển qua lại giữa 2
- * thành phố mà nó kết nối. Bờm sẽ đi qua các tuyến đường cao tốc nhất định để từ thành phố 1 về đến nhà ở thành phố N.
+ * Ada the Ladybug loves trips. She travels around world taking photos and souvenirs. This week she went to Buganda. Common Tourist
+ * would surely travel around main city and some conurbations, but Ada has different politics. She wants to go as far as possible
+ * (because photos from outlying places are much more valuable).
  *
- * Thông thường, khi đi trên tuyến đường cao tốc này, người ta phải trả chi phí bằng tổng độ dài của các tuyến đường cao tốc mà họ đi qua. Tuy
- * nhiên, do Bờm là sinh viên nên sẽ được ưu tiên tính theo cách đặc biệt khác, chi phí mà Bờm phải trả khi đi từ thành phố 1 đến thành phố N
- * bằng với độ dài của tuyến đường cao tốc dài thứ nhì trong số các tuyến đường cao tốc mà Bờm đi qua. Đặc biệt nếu Bờm chỉ đi qua 1 tuyến đường
- * duy nhất thì chi phí mà Bờm phải trả là 0.
- *
- * Ví dụ: Với sơ đồ bên dưới, để đi từ thành phố 1 đến thành phố 4. Nếu Bờm đi theo thứ tự 1->2->4 thì độ dài tuyến đường đi qua lần lượt là 8, 7,
- * Bờm phải trả chi phí là 7; Nếu Bờm đi theo thứ tự 1->3->4 thì độ dài tuyến đường đi qua lần lượt là 2, 100, Bờm phải trả chi phí là 2.
- * (~/images/000065_trip_01.png)
- *
- * Do là sinh viên nên Bờm không có nhiều tiền, hãy giúp Bờm chọn tuyến đường đi sao cho chi phí phải trả là nhỏ nhất. Hoặc thông báo nếu không
- * có tuyến đường nào để đi từ thành phố 1 đến thành phố N.
+ * Problem is, that Buganda is very large so she can barely guess such places. Luckily, you are around so she asked you for help.
+ * Can you tell her, how far and how many cities are most distant (if the shortest path is used)?
  *
  * Input:
- * Dòng đầu tiên chứa 2 số N, M
- * M dòng tiếp theo, mỗi dòng gồm 3 số x, y, w, thể hiện có một tuyến đường cao tốc nối giữa thành phố x và thành phố y với độ dài w. Giữa mỗi cặp
- * thành phố chỉ có duy nhất 1 tuyến đường nối giữa chúng. Không có tuyến đường nào nối một thành phố với chính nó.
+ * The first line will contain three integers 1≤N≤5⋅10^5​​, 0≤M≤10^6, Q, the number of cities in Buganda, the number of roads and number
+ * of queries (possible arrival cities).
  *
- * Constraints
- *      2≤N≤10^5
- *      0≤M≤10^5
- *      1≤x,y≤N
- *      1≤w≤10^9
+ * Then M lines follow, with three integers 0≤A,B<N,0≤L≤10,A,B are cities, which the (bidirectional) road connets and LL is length of the road.
+
+Afterward, QQ lines follow, each with number 0≤qi<N0≤q​i​​<N, meaning the city of arival.
+
+You are asured that maxmax(NN, MM) * QQ will be always lesser/equal than 10710​7​​
+
+Gentle warning: Since we are in real world and not in some "graph theory", multiedges and self-edges are completely valid!
+
  *
  * Output:
- * Một số nguyên duy nhất là chi phí mà Bờm phải trả. Nếu không tồn tại cách đi để Bờm đến được thành phố N thì xuất ra -1
+ * For each query print two numbers: The distance of most distant place(s) and number of such places.
  *
  * Example 1:
  * Input:
- * 4 4
- * 1 2 8
- * 2 4 7
- * 1 3 2
- * 3 4 1000
+ * 10 10 10
+1 1 1
+1 2 1
+1 2 3
+3 1 1
+5 4 10
+8 5 10
+5 6 5
+6 7 3
+6 9 3
+9 7 4
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
  *
  * Output:
- * 2
- *
- * Example 2:
- * Input:
- * 2 1
- * 1 2 1000
- *
- * Output:
- * 0
+ * 0 1
+1 2
+2 1
+2 1
+20 1
+10 2
+15 2
+18 2
+20 1
+18 2
+
+Explanation:
+ Giải thích test ví dụ
+
+Most distant cities (explanation)
+0
+2 3
+3
+2
+8
+4 8
+4 8
+4 8
+4
+4 8
+
+
  **/
 
 #include <iostream>
