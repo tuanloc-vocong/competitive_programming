2296be92e31d7bcd434b01acdef6add781f7eeda
vocongtuanloc <vocong.tuanloc@gmail.com>
2024-05-27 17:16:09 +0700

    000068 missile silos

--- cplusplus/000066_ada_and_trip.cpp
+++ cplusplus/000066_ada_and_trip.cpp
@@ -19,14 +19,13 @@
  * The first line will contain three integers 1≤N≤5⋅10^5​​, 0≤M≤10^6, Q, the number of cities in Buganda, the number of roads and number
  * of queries (possible arrival cities).
  *
- * Then M lines follow, with three integers 0≤A,B<N,0≤L≤10,A,B are cities, which the (bidirectional) road connets and LL is length of the road.
-
-Afterward, QQ lines follow, each with number 0≤qi<N0≤q​i​​<N, meaning the city of arival.
-
-You are asured that maxmax(NN, MM) * QQ will be always lesser/equal than 10710​7​​
-
-Gentle warning: Since we are in real world and not in some "graph theory", multiedges and self-edges are completely valid!
-
+ * Then M lines follow, with three integers 0≤A,B<N,0≤L≤10,A,B are cities, which the (bidirectional) road connets and L is length of the road.
+ *
+ * Afterward, Q lines follow, each with number 0≤qi<N, meaning the city of arival.
+ *
+ * You are asured that max(N, M) * Q will be always lesser/equal than 10^7
+ *
+ * Gentle warning: Since we are in real world and not in some "graph theory", multiedges and self-edges are completely valid!
  *
  * Output:
  * For each query print two numbers: The distance of most distant place(s) and number of such places.
@@ -34,56 +33,52 @@ Gentle warning: Since we are in real world and not in some "graph theory", multi
  * Example 1:
  * Input:
  * 10 10 10
-1 1 1
-1 2 1
-1 2 3
-3 1 1
-5 4 10
-8 5 10
-5 6 5
-6 7 3
-6 9 3
-9 7 4
-0
-1
-2
-3
-4
-5
-6
-7
-8
-9
-
+ * 1 1 1
+ * 1 2 1
+ * 1 2 3
+ * 3 1 1
+ * 5 4 10
+ * 8 5 10
+ * 5 6 5
+ * 6 7 3
+ * 6 9 3
+ * 9 7 4
+ * 0
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * 6
+ * 7
+ * 8
+ * 9
  *
  * Output:
  * 0 1
-1 2
-2 1
-2 1
-20 1
-10 2
-15 2
-18 2
-20 1
-18 2
-
-Explanation:
- Giải thích test ví dụ
-
-Most distant cities (explanation)
-0
-2 3
-3
-2
-8
-4 8
-4 8
-4 8
-4
-4 8
-
-
+ * 1 2
+ * 2 1
+ * 2 1
+ * 20 1
+ * 10 2
+ * 15 2
+ * 18 2
+ * 20 1
+ * 18 2
+ *
+ * Explanation:
+ * Giải thích test ví dụ
+ * Most distant cities (explanation)
+ * 0
+ * 2 3
+ * 3
+ * 2
+ * 8
+ * 4 8
+ * 4 8
+ * 4 8
+ * 4
+ * 4 8
  **/
 
 #include <iostream>
